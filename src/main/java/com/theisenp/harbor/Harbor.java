1af232a05421bc25b6aea0fc524bd613e2c83642
Patrick Theisen <patrick.theisen@soartech.com>
2014-10-25 16:38:35 -0400

    Added initial Harbor construction/builder/validation and tests

---
+++ src/main/java/com/theisenp/harbor/Harbor.java
@@ -0,0 +1,183 @@
+package com.theisenp.harbor;
+
+import org.joda.time.Duration;
+
+import com.theisenp.harbor.utils.HarborUtils;
+
+/**
+ * An LCM based peer discovery utility
+ * 
+ * @author patrick.theisen
+ */
+public class Harbor {
+	public static final String DEFAULT_ADDRESS = "239.255.76.67";
+	public static final int DEFAULT_PORT = 7667;
+	public static final int DEFAULT_TTL = 0;
+	public static final Duration DEFAULT_PERIOD = Duration.standardSeconds(1);
+	public static final Duration DEFAULT_TIMEOUT = Duration.standardSeconds(5);
+
+	private final String address;
+	private final int port;
+	private final int ttl;
+	private final Duration period;
+	private final Duration timeout;
+
+	/**
+	 * @param address
+	 * @param port
+	 * @param ttl
+	 * @param periodMillis
+	 * @param timeoutMillis
+	 */
+	public Harbor(String address, int port, int ttl, Duration period, Duration timeout) {
+		this.address = address;
+		this.port = port;
+		this.ttl = ttl;
+		this.period = period;
+		this.timeout = timeout;
+
+		HarborUtils.validateAddress(address);
+		HarborUtils.validatePort(port);
+		HarborUtils.validateTtl(ttl);
+		HarborUtils.validatePeriod(period);
+		HarborUtils.validateTimeout(timeout);
+	}
+
+	/**
+	 * @return
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @return
+	 */
+	public int getPort() {
+		return port;
+	}
+
+	/**
+	 * @return
+	 */
+	public int getTtl() {
+		return ttl;
+	}
+
+	/**
+	 * @return
+	 */
+	public Duration getPeriod() {
+		return period;
+	}
+
+	/**
+	 * @return
+	 */
+	public Duration getTimeout() {
+		return timeout;
+	}
+
+	/**
+	 * A fluent builder for {@link Harbor}
+	 * 
+	 * @author patrick.theisen
+	 */
+	public static class Builder {
+		private String address = DEFAULT_ADDRESS;
+		private int port = DEFAULT_PORT;
+		private int ttl = DEFAULT_TTL;
+		private Duration period = DEFAULT_PERIOD;
+		private Duration timeout = DEFAULT_TIMEOUT;
+
+		/**
+		 * 
+		 */
+		public Builder() {
+		}
+
+		/**
+		 * @param other
+		 */
+		public Builder(Harbor other) {
+			this.address = other.address;
+			this.port = other.port;
+			this.ttl = other.ttl;
+			this.period = other.period;
+			this.timeout = other.timeout;
+		}
+
+		/**
+		 * @param address
+		 * @return This instance
+		 */
+		public Builder address(String address) {
+			HarborUtils.validateAddress(address);
+			this.address = address;
+			return this;
+		}
+
+		/**
+		 * @param port
+		 * @return This instance
+		 */
+		public Builder port(int port) {
+			HarborUtils.validatePort(port);
+			this.port = port;
+			return this;
+		}
+
+		/**
+		 * @param ttl
+		 * @return This instance
+		 */
+		public Builder ttl(int ttl) {
+			HarborUtils.validateTtl(ttl);
+			this.ttl = ttl;
+			return this;
+		}
+
+		/**
+		 * @param period
+		 * @return This instance
+		 */
+		public Builder period(Duration period) {
+			HarborUtils.validatePeriod(period);
+			this.period = period;
+			return this;
+		}
+
+		/**
+		 * @param timeout
+		 * @return This instance
+		 */
+		public Builder timeout(Duration timeout) {
+			HarborUtils.validateTimeout(timeout);
+			this.timeout = timeout;
+			return this;
+		}
+
+		/**
+		 * Clears any previously set parameters
+		 * 
+		 * @return This instance
+		 */
+		public Builder reset() {
+			address = DEFAULT_ADDRESS;
+			port = DEFAULT_PORT;
+			ttl = DEFAULT_TTL;
+			period = DEFAULT_PERIOD;
+			timeout = DEFAULT_TIMEOUT;
+			return this;
+		}
+
+		/**
+		 * A {@link Harbor} built from the current state
+		 * 
+		 * @return
+		 */
+		public Harbor build() {
+			return new Harbor(address, port, ttl, period, timeout);
+		}
+	}
+}
---
+++ src/test/java/com/theisenp/harbor/HarborTest.java
@@ -0,0 +1,151 @@
+package com.theisenp.harbor;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.joda.time.Duration;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.theisenp.harbor.Harbor.Builder;
+
+/**
+ * Unit tests for {@link Harbor}
+ * 
+ * @author patrick.theisen
+ */
+public class HarborTest {
+	private static final String TEST_ADDRESS = "0.0.0.0";
+	private static final int TEST_PORT = 1;
+	private static final int TEST_TTL = 1;
+	private static final Duration TEST_PERIOD = Duration.standardSeconds(1);
+	private static final Duration TEST_TIMEOUT = Duration.standardSeconds(1);
+
+	@Rule
+	public final ExpectedException thrown = ExpectedException.none();
+
+	@Test
+	public void testConstruct() {
+		validate(new Harbor(TEST_ADDRESS, TEST_PORT, TEST_TTL, TEST_PERIOD, TEST_TIMEOUT));
+	}
+
+	@Test
+	public void testConstructWithInvalidAddress() {
+		thrown.expect(IllegalArgumentException.class);
+		new Harbor("invalid", TEST_PORT, TEST_TTL, TEST_PERIOD, TEST_TIMEOUT);
+	}
+
+	@Test
+	public void testConstructWithInvalidPort() {
+		thrown.expect(IllegalArgumentException.class);
+		new Harbor(TEST_ADDRESS, 0, TEST_TTL, TEST_PERIOD, TEST_TIMEOUT);
+	}
+
+	@Test
+	public void testConstructWithInvalidTtl() {
+		thrown.expect(IllegalArgumentException.class);
+		new Harbor(TEST_ADDRESS, TEST_PORT, -1, TEST_PERIOD, TEST_TIMEOUT);
+	}
+
+	@Test
+	public void testConstructWithInvalidPeriod() {
+		thrown.expect(IllegalArgumentException.class);
+		new Harbor(TEST_ADDRESS, TEST_PORT, TEST_TTL, Duration.ZERO, TEST_TIMEOUT);
+	}
+
+	@Test
+	public void testConstructWithInvalidTimeout() {
+		thrown.expect(IllegalArgumentException.class);
+		new Harbor(TEST_ADDRESS, TEST_PORT, TEST_TTL, TEST_PERIOD, Duration.ZERO);
+	}
+
+	@Test
+	public void testBuild() {
+		Builder builder = new Builder();
+		builder.address(TEST_ADDRESS);
+		builder.port(TEST_PORT);
+		builder.ttl(TEST_TTL);
+		builder.period(TEST_PERIOD);
+		builder.timeout(TEST_TIMEOUT);
+		validate(builder.build());
+	}
+
+	@Test
+	public void testBuildCopy() {
+		Harbor original = new Harbor(TEST_ADDRESS, TEST_PORT, TEST_TTL, TEST_PERIOD, TEST_TIMEOUT);
+		validate(new Builder(original).build());
+	}
+
+	@Test
+	public void testBuildDefault() {
+		validateDefault(new Builder().build());
+	}
+
+	@Test
+	public void testBuildReset() {
+		Builder builder = new Builder();
+		builder.address(TEST_ADDRESS);
+		builder.port(TEST_PORT);
+		builder.ttl(TEST_TTL);
+		builder.period(TEST_PERIOD);
+		builder.timeout(TEST_TIMEOUT);
+		builder.reset();
+		validateDefault(builder.build());
+	}
+
+	@Test
+	public void testBuildWithInvalidAddress() {
+		thrown.expect(IllegalArgumentException.class);
+		new Builder().address("invalid");
+	}
+
+	@Test
+	public void testBuildWithInvalidPort() {
+		thrown.expect(IllegalArgumentException.class);
+		new Builder().port(0);
+	}
+
+	@Test
+	public void testBuildWithInvalidTtl() {
+		thrown.expect(IllegalArgumentException.class);
+		new Builder().ttl(-1);
+	}
+
+	@Test
+	public void testBuildWithInvalidPeriod() {
+		thrown.expect(IllegalArgumentException.class);
+		new Builder().period(Duration.ZERO);
+	}
+
+	@Test
+	public void testBuildWithInvalidTimeout() {
+		thrown.expect(IllegalArgumentException.class);
+		new Builder().timeout(Duration.ZERO);
+	}
+
+	/**
+	 * Verify that the given {@link Harbor} has the expected test data
+	 * 
+	 * @param harbor
+	 */
+	private static void validate(Harbor harbor) {
+		assertThat(harbor.getAddress()).isEqualTo(TEST_ADDRESS);
+		assertThat(harbor.getPort()).isEqualTo(TEST_PORT);
+		assertThat(harbor.getTtl()).isEqualTo(TEST_TTL);
+		assertThat(harbor.getPeriod()).isEqualTo(TEST_PERIOD);
+		assertThat(harbor.getTimeout()).isEqualTo(TEST_TIMEOUT);
+	}
+
+	/**
+	 * Verify that the given {@link Harbor} has the expected test data
+	 * 
+	 * @param harbor
+	 */
+	private static void validateDefault(Harbor harbor) {
+		assertThat(harbor.getAddress()).isEqualTo(Harbor.DEFAULT_ADDRESS);
+		assertThat(harbor.getPort()).isEqualTo(Harbor.DEFAULT_PORT);
+		assertThat(harbor.getTtl()).isEqualTo(Harbor.DEFAULT_TTL);
+		assertThat(harbor.getPeriod()).isEqualTo(Harbor.DEFAULT_PERIOD);
+		assertThat(harbor.getTimeout()).isEqualTo(Harbor.DEFAULT_TIMEOUT);
+	}
+}
