dff9a3c072d9fa785173fe30ee08fdf22191bbbb
Patrick Theisen <patrick.theisen@soartech.com>
2014-10-25 20:13:56 -0400

    Added InitializeLcm callable

---
+++ src/main/java/com/theisenp/harbor/lcm/InitializeLcm.java
@@ -0,0 +1,34 @@
+package com.theisenp.harbor.lcm;
+
+import java.util.concurrent.Callable;
+
+import lcm.lcm.LCM;
+
+import com.theisenp.harbor.utils.HarborUtils;
+
+/**
+ * A {@link Callable} that produces an {@link LCM} instance
+ * 
+ * @author patrick.theisen
+ */
+public class InitializeLcm implements Callable<LCM> {
+	private final String address;
+	private final int port;
+	private final int ttl;
+
+	/**
+	 * @param address
+	 * @param port
+	 * @param ttl
+	 */
+	public InitializeLcm(String address, int port, int ttl) {
+		this.address = address;
+		this.port = port;
+		this.ttl = ttl;
+	}
+
+	@Override
+	public LCM call() throws Exception {
+		return new LCM(HarborUtils.toLcmAddress(address, port, ttl));
+	}
+}
